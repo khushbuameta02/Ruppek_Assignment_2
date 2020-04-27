0c557d7fbefc0ad092c916f9e437304b37590369
khushbuameta02 <59528794+khushbuameta02@users.noreply.github.com>
2020-04-27 16:14:49 +0530

    Update IAutho.java

--- src/main/java/com/rupeek/genericfiles/IAutho.java
+++ src/main/java/com/rupeek/genericfiles/IAutho.java
@@ -5,6 +5,6 @@ public interface IAutho {
 	String TOKEN="Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJydXBlZWsiLCJleHAiOjE1ODc5ODQxMjEsImlhdCI6MTU4Nzk2NjEyMX0.I72KlUzndS8GrB4yuIhgQfaf7TBYhK84mHmG0khxMSB4Ld7l-PR5O-v-HyVEGImgxIEtyWLOBkSB8G8qthcYGQ";
 
 	String USER_ENDPOINT="http://13.126.80.194:8080/api/v1/users";
-	String USER_PHONE="http://13.126.80.194:8080/api/v1";
+	String USER_PHONE="http://13.126.80.194:8080/api/v1/{phone}";
 	
 }
